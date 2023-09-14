ac93df88236ec05d885c8641bd9decfe2cf9012a
Ahmadsul5 <ahmad.abo.sultan2001@gmail.com>
2023-09-15 01:10:13 +0300

    js

---
+++ main.js
@@ -0,0 +1,272 @@
+// window.alert("hello");
+// document.write("<h1>hello</h1>")
+// console.log("hello ")
+// console.table(["ahmad", "ali"])
+// console.log({ name: "ahamd", age: "19" })
+
+// var user = "ahmad";
+// console.log(user);
+// hello.innerHTML = "hi"
+// console.log("\"ahmad\\ \nsultan")
+// let a = "ahmad"
+// let b = "sultan"
+// document.write(a + " " + b)
+// let ht = `
+
+//     <div>
+//         <h1>${a} is the king </h1>
+//     </div>
+
+// `;
+// document.write(ht);
+
+// let titelContent = "ahmad"
+//     , descriptiinContent = "ahmad web school"
+//     , date = "13\\10";
+
+// let content = `
+// <div>
+// <h3>${titelContent}</h3>
+// <p> ${descriptiinContent} </p>
+// <span> ${date} </span>
+
+// </div>
+// <div>
+// <h3>${titelContent}</h3>
+// <p> ${descriptiinContent} </p>
+// <span> ${date} </span>
+
+// </div>
+// <div>
+// <h3>${titelContent}</h3>
+// <p> ${descriptiinContent} </p>
+// <span> ${date} </span>
+
+// </div>
+// <div>
+// <h3>${titelContent}</h3>
+// <p> ${descriptiinContent} </p>
+// <span> ${date} </span>
+
+// </div>
+
+// `;
+// document.write(content);
+
+
+// console.log(10 + "ahmad")
+// console.log(10 - "ahmad")
+// console.log(typeof NaN)
+// console.log(3 ** 3)
+// console.log(11 % 2)
+// console.log(++1)
+// console.log(100)
+// console.log("100")
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(10 === "10")
+// console.log(10 < 20)
+
+
+// console.log(!true)
+// console.log(10 == 10 && 10 > 20)
+// console.log(10 == 10 || 10 > 20)
+// ////////////////////////////////////////////
+// let price = 100;
+// let discount = false;
+// let discountAmount = 30;
+// let contry = "palestine";
+// let student = true;
+
+// if (discount === true) {
+//     price = price - discountAmount;
+
+// } else if (contry === "palestine") {
+
+
+//     if (student == true) {
+//         price -= discountAmount + 30;
+//     } else {
+//         price -= discountAmount + 10;
+
+//     }
+
+// } else {
+//     price = price - 10
+// }
+// console.log(price)
+// ///////////////////////////////////////
+
+
+// let day = 2
+
+// switch (day) {
+//     case 0:
+//         console.log("its 0")
+//         break;
+//     case 1:
+//         console.log("its 1")
+//         break;
+//     case 2:
+//         console.log("its 2")
+//         break;
+//     default:
+//         console.log(" unknow")
+
+
+// }
+
+// let job = "manager";
+// let salary = 0;
+// switch (job) {
+//     case "manager":
+//         salary = 8000
+//         console.log(salary)
+//         break;
+//     case "it":
+//     case "support":
+//         salary = 6000
+//         console.log(salary)
+//         break;
+//     case "developer":
+//     case "desiner":
+//         salary = 6000
+//         console.log(salary)
+//         break;
+//     default:
+//         salary = 4000
+//         console.log(salary)
+// }
+
+// let holeday = 3;
+// let money = 0;
+
+// if (holeday == 0) {
+//     money = 5000
+//     console.log(`my mony ${money}`)
+// } else if (holeday == 1 && 2) {
+//     money = 3000
+//     console.log(`my mony ${money}`)
+
+// } else if (holeday == 3) {
+//     money = 2000
+//     console.log(`my mony ${money}`)
+
+// } else {
+//     console.log(`my mony ${money}`)
+
+// }
+
+// let myFrend = ["ahmad", "moh", "ali"]
+// console.log(`hello ${myFrend[0]}`)
+// console.log(`hello ${myFrend[0][0]}`)
+// console.log(myFrend);
+// myFrend[0] = "mohmaed";
+// myFrend[myFrend.length] = "mohmaed";
+// console.log(myFrend.length);
+// myFrend.length = 9
+
+// console.log(myFrend);
+// let frends = [ "salah","ahmad","ali",1000 ,"200"];
+// console.log(frends);
+// frends.unshift("sultan");
+// console.log(frends);
+// frends.push("abo");
+// console.log(frends);
+// frends.shift();
+// console.log(frends);
+// frends.pop();
+// console.log(frends);
+
+// console.log(frends .indexOf("ali"));
+// console.log(frends .lastIndexOf("salah"));
+// console.log(frends.sort());
+// console.log(frends.reverse());
+
+// let myFriend =["ahmad","salah","moh"];
+// let myNewFriend=["yosef",'ayad'];
+// let myschoolfriends =["fady", "ali"]
+
+// let allfriend =myFriend.concat(myNewFriend , myschoolfriends);
+
+// console.log(allfriend);
+// console.log(allfriend.join(" $$ "));
+
+// let zero = 0; 
+// let counter = 3;
+// let my =["ahmad","mazero","elham","osama","gamal","ameer"];
+// console.log(my.slice(0,4).reverse());
+// console.log(my.slice(++zero,counter).sort());
+// console.log(my[2][0]+[1]+my[1].slice(2))
+// console.log(my[2][1])
+// console.log(my.splice(zero,(counter+counter),"elzero"));
+
+
+// for (let i=1; i<10;i++){
+// console.log(i);
+// }
+
+// let myFrend = ["ahmad", "moh", "ali"];
+
+
+// for (let i=0; i<myFrend.length;i++){
+//     console.log(myFrend[i]);
+//     }
+
+// let prodect =["key","mouse","pen","pad","iphone", "ahmad"];
+// let showCount = 3;
+
+// document.write(`<h1>show ${showCount} prodect</h1>`)
+
+// for (let i=0 ; i<showCount;i++){
+//     document.write(`<div>`);
+//     document.write(`<h3>[${i+1}]${prodect[i]} </h3>`);
+
+//     document.write(`</div>`);
+// }
+
+
+// let i=0;
+// while(i<prodect.length){
+//     console.log(prodect[i]);
+//     i++;
+// }
+// do{
+//     console.log(prodect[i]);
+//     i++;
+// }while(i<prodect.length)
+
+// function sayhello (username,age){
+//     console.log(`hello ${username} your age ${age}`);
+//     if (age <20){
+//         console.log(`this app is not for you`)
+//     }else{
+//         console.log(`hello ${username} your age ${age}`)
+//     }
+// }
+// sayhello("ahmad","50");
+// sayhello("ahmad","18");
+// sayhello("ahmad","20");
+// sayhello("ahmad","18");
+
+// function yerrs(start,end,remove){
+//     for(let i =start ; i<=end ; i++){
+
+//         if( i=== remove){
+//             continue;
+            
+//         }
+//         console.log(i);
+//     }
+    
+// }
+
+// yerrs(2000,2022,2010);
+
+// function calc(num1,num2){
+//     return num1 +num2 ;
+// }
+// let result = calc(10,20)
+// console.log(result+100)
+
+
